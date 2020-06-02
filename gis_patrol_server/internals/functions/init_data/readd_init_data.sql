461bf6d4e0922a001a81bcc3a690bc452a3f03fc
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-06-02 15:17:10 +0300

    Initial parameters were set.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_server/internals/functions/init_data/readd_init_data.sql
+++ gis_patrol_server/internals/functions/init_data/readd_init_data.sql
@@ -1,3 +1,6 @@
+begin;
 \i ./functions/init_data/param_types.sql
 \i ./functions/init_data/category_types.sql
+\i ./functions/init_data/parameters.sql
+commit;
 \i ./functions/init_data/maclabels.sql
