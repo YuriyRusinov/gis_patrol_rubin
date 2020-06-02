7263320ae022def0d2c03cf145e2f4a93d72be8f
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-06-02 18:17:51 +0300

    Some system categories were added.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_server/internals/functions/init_data/readd_init_data.sql
+++ gis_patrol_server/internals/functions/init_data/readd_init_data.sql
@@ -2,5 +2,6 @@ begin;
 \i ./functions/init_data/param_types.sql
 \i ./functions/init_data/category_types.sql
 \i ./functions/init_data/parameters.sql
+\i ./functions/init_data/categories.sql
 commit;
 \i ./functions/init_data/maclabels.sql
