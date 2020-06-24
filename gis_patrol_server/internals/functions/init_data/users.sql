1fbee6e9bd8aa6b81acfaafa3c59227fb3cdf117
Yury Rusinov <yrusinov@gmail.com>
2020-06-24 22:03:26 +0300

    Merge postgres into master.
    Signed-off-by: Yury Rusinov <yrusinov@gmail.com>

--- gis_patrol_server/internals/functions/init_data/users.sql
+++ gis_patrol_server/internals/functions/init_data/users.sql
@@ -1,7 +1,7 @@
 /*
 Системные пользователи
 */
-select safeCreateUser('yuriy', 'yuriy', true);
-insert into users(id, id_maclabel, firstname, surname, lastname, family_name, information, email, db_user) values(1, 1, 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yuriy');
+select safeCreateUser('yrusinov', 'yrusinov', true);
+insert into users(id, id_maclabel, firstname, surname, lastname, family_name, information, email, db_user) values(1, 1, 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yrusinov');
 
 select setval('users_id_seq', 1, true);
