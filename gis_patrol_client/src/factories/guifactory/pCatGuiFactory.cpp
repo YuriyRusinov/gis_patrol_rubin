861b796eb0e056cb1c9ab2688a35c2c60c3d8a2a
Yury Rusinov <yrusinov@gmail.com>
2020-06-24 21:38:31 +0300

    View categories, signals for add-edit-del.
    Signed-off-by: Yury Rusinov <yrusinov@gmail.com>

--- gis_patrol_client/src/factories/guifactory/pCatGuiFactory.cpp
+++ gis_patrol_client/src/factories/guifactory/pCatGuiFactory.cpp
@@ -11,6 +11,7 @@
 #include <patroldbwriter.h>
 #include <pCategoryListForm.h>
 #include <pCategoryModel.h>
+#include <pCategory.h>
 
 #include "pCatGuiFactory.h"
 
@@ -23,9 +24,29 @@ pCatGuiFactory::~pCatGuiFactory() {
 
 QWidget* pCatGuiFactory::GUICatView( QWidget* parent, Qt::WindowFlags flags ) {
     pCategoryListForm* pclf = new pCategoryListForm( parent, flags );
+    QObject::connect( pclf, &pCategoryListForm::addCategory, this, &pCatGuiFactory::addPCategory );
+    QObject::connect( pclf, &pCategoryListForm::editCategory, this, &pCatGuiFactory::editPCategory );
+    QObject::connect( pclf, &pCategoryListForm::delCategory, this, &pCatGuiFactory::delPCategory );
+    QObject::connect( pclf, &pCategoryListForm::refrechCatMod, this, &pCatGuiFactory::refreshCats );
     QMap< qint64, QSharedPointer< pCategory > > categories = _dbLoader->loadCategories();
     qDebug() << __PRETTY_FUNCTION__ << categories.size();
     pCategoryModel* pCatMod = new pCategoryModel( categories );
     pclf->setCatModel( pCatMod );
     return pclf;
+}
+
+void pCatGuiFactory::addPCategory( QAbstractItemModel* catMod ) {
+    qDebug() << __PRETTY_FUNCTION__ << catMod;
+}
+
+void pCatGuiFactory::editPCategory( QAbstractItemModel* catMod, QSharedPointer< pCategory > pCat, QModelIndex cIndex ) {
+    qDebug() << __PRETTY_FUNCTION__ << catMod << pCat.isNull() << cIndex;
+}
+
+void pCatGuiFactory::delPCategory( QAbstractItemModel* catMod, QModelIndex cIndex ) {
+    qDebug() << __PRETTY_FUNCTION__ << catMod << cIndex;
+}
+
+void pCatGuiFactory::refreshCats() {
+    qDebug() << __PRETTY_FUNCTION__;
 }
--- gis_patrol_client/src/factories/guifactory/pCatGuiFactory.h
+++ gis_patrol_client/src/factories/guifactory/pCatGuiFactory.h
@@ -10,16 +10,26 @@
 #pragma once
 
 #include <QModelIndex>
+#include <QSharedPointer>
 #include <QObject>
 
 class QAbstractItemModel;
 class QWidget;
+
 class pDBLoader;
 class pDBWriter;
+
+class pCategory;
 
 class pCatGuiFactory : public QObject {
 public:
     QWidget* GUICatView( QWidget* parent=nullptr, Qt::WindowFlags flags=Qt::WindowFlags() );
+
+private slots:
+    void addPCategory( QAbstractItemModel* catMod );
+    void editPCategory( QAbstractItemModel* catMod, QSharedPointer< pCategory > pCat, QModelIndex cIndex );
+    void delPCategory( QAbstractItemModel* catMod, QModelIndex cIndex );
+    void refreshCats();
 
 private:
     pCatGuiFactory( pDBLoader* dbLoader, pDBWriter* dbWriter,  QObject* parent=nullptr );
