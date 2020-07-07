fa60d6bd1aa4b39d1004302681408695ca27e93a
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-07-07 18:59:56 +0300

    Base class of records was added.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_client/src/patrolCommunications/pRecord.cpp
+++ gis_patrol_client/src/patrolCommunications/pRecord.cpp
@@ -9,19 +9,113 @@
 
 #include "pRecord.h"
 
-pRecord::pRecord() {
+pRecord::pRecord()
+    : _id ( -1 ),
+    _uuid( QString() ),
+    _icon( QIcon() ),
+    _recordFillColor( QColor() ),
+    _recordTextColor( QColor() ),
+    _name( QString() ),
+    _code( QString() ),
+    _description( QString() ),
+    _parent( nullptr ) {
 }
 
-pRecord::pRecord( qint64 id, const QString& name, const QString& desc, const QString& code ) {
-    Q_UNUSED( id );
-    Q_UNUSED( name );
-    Q_UNUSED( desc );
-    Q_UNUSED( code );
+pRecord::pRecord( qint64 id, const QString& name, const QString& desc, const QString& code )
+    : _id( id ),
+    _uuid( QString() ),
+    _icon( QIcon() ),
+    _recordFillColor( QColor() ),
+    _recordTextColor( QColor() ),
+    _name( name ),
+    _code( code ),
+    _description( desc ),
+    _parent( nullptr ) {
 }
 
-pRecord::pRecord( const pRecord& r ) {
-    Q_UNUSED( r );
+pRecord::pRecord( const pRecord& r )
+    : _id( r._id ),
+    _uuid( r._uuid ),
+    _icon( r._icon ),
+    _recordFillColor( r._recordFillColor ),
+    _recordTextColor( r._recordTextColor ),
+    _name( r._name ),
+    _code( r._code ),
+    _description( r._description ),
+    _parent( r._parent ) {
 }
 
 pRecord::~pRecord() {
+}
+
+qint64 pRecord::getId() const {
+    return _id;
+}
+
+void pRecord::setId( qint64 id ) {
+    _id = id;
+}
+
+const QString& pRecord::getUUID() const {
+    return _uuid;
+}
+
+void pRecord::setUUID( const QString& u ) {
+    _uuid = u;
+}
+
+const QIcon& pRecord::getIcon() const {
+    return _icon;
+}
+
+void pRecord::setIcon( const QIcon& i ) {
+    _icon = i;
+}
+
+const QColor& pRecord::getFillColor() const {
+    return _recordFillColor;
+}
+
+void pRecord::setFillColor( const QColor& fc ) {
+    _recordFillColor = fc;
+}
+
+const QColor& pRecord::getTextColor() const {
+    return _recordTextColor;
+}
+
+void pRecord::setTextColor( const QColor& tc ) {
+    _recordTextColor = tc;
+}
+
+const QString& pRecord::getName() const {
+    return _name;
+}
+
+void pRecord::setName( const QString& name ) {
+    _name = name;
+}
+
+const QString& pRecord::getCode() const {
+    return _code;
+}
+
+void pRecord::setCode( const QString& code ) {
+    _code = code;
+}
+
+const QString& pRecord::getDesc() const {
+    return _description;
+}
+
+void pRecord::setDesc( const QString& desc ) {
+    _description = desc;
+}
+
+void pRecord::setParent( QSharedPointer< pRecord > p ) {
+    _parent = p;
+}
+
+QSharedPointer< pRecord > pRecord::getParent() const {
+    return _parent;
 }
--- gis_patrol_client/src/patrolCommunications/pRecord.h
+++ gis_patrol_client/src/patrolCommunications/pRecord.h
@@ -9,6 +9,9 @@
 
 #pragma	once
 
+#include <QIcon>
+#include <QColor>
+#include <QSharedPointer>
 #include <QString>
 
 class pRecord {
@@ -17,4 +20,45 @@ public:
     pRecord( qint64 id, const QString& name, const QString& desc=QString(), const QString& code = QString() );
     pRecord( const pRecord& r );
     ~pRecord();
+
+    qint64 getId() const;
+    void setId( qint64 id );
+
+    const QString& getUUID() const;
+    void setUUID( const QString& u );
+
+    const QIcon& getIcon() const;
+    void setIcon( const QIcon& i );
+
+    const QColor& getFillColor() const;
+    void setFillColor( const QColor& fc );
+
+    const QColor& getTextColor() const;
+    void setTextColor( const QColor& tc );
+
+    const QString& getName() const;
+    void setName( const QString& name );
+
+    const QString& getCode() const;
+    void setCode( const QString& code );
+
+    const QString& getDesc() const;
+    void setDesc( const QString& desc );
+
+    virtual void setParent( QSharedPointer< pRecord > p );
+    QSharedPointer< pRecord > getParent() const;
+
+private:
+    qint64 _id;// рабочий идентификатор записи, который используется в системе поддержания ссылочной целостности (первичный ключ)
+    QString _uuid; // уникальый идентификатор записи в системе генерации уникальных идентификаторов uuid-ossp
+    QIcon _icon;// иконка для каждой записи из несистемного справочника. Для системных справочников, а также для тех, где иконка не задана приобретает значение null
+
+    QColor _recordFillColor;// цвет фона при отображении записи в табличном виде
+    QColor _recordTextColor;// цвет текста при отображении записи в табличном виде
+
+    QString _name;
+    QString _code;
+    QString _description;
+
+    QSharedPointer< pRecord > _parent;
 };
