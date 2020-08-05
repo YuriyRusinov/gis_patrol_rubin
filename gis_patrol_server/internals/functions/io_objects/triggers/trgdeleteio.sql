ad36b00260ea311ffe1ef69b850f32a4d9242a05
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-08-05 17:53:07 +0300

    Trigger was corrected.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_server/internals/functions/io_objects/triggers/trgdeleteio.sql
+++ gis_patrol_server/internals/functions/io_objects/triggers/trgdeleteio.sql
@@ -7,17 +7,10 @@ begin
 
     table_name := old.table_name;
     if (table_name is not null and table_name <> '') then
-        if(old.ref_table_name is not null and old.ref_table_name <> '') then
-            query = 'select f_safe_drop_table(' || quote_literal(table_name) || ')';
-            execute query;
-            query = 'select f_safe_drop_table(' || quote_literal('tbl_' || table_name) || ')';
-            execute query;
-        else
-            query = 'select f_safe_drop_table(' || quote_literal(table_name) || ')';
-            execute query;
-            query = 'select f_safe_drop_table(' || quote_literal('tbl_' || table_name) || ')';
-            execute query;
-        end if;
+        query = 'select f_safe_drop_table(' || quote_literal(table_name) || ')';
+        execute query;
+        query = 'select f_safe_drop_table(' || quote_literal('tbl_' || table_name) || ')';
+        execute query;
         query = 'drop sequence ' || table_name || '_id_seq';
         execute query;
         --raise notice '%', query;
