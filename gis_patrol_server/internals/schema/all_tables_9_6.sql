4b87e99eca0be68a01bdd28ca4fcd6d1e0ed1205
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-09-03 14:42:54 +0300

    Some table indexes were inserted.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_server/internals/schema/all_tables_9_6.sql
+++ gis_patrol_server/internals/schema/all_tables_9_6.sql
@@ -293,6 +293,70 @@ CREATE TABLE public.tbl_parameters_groups (
 );
 -- ddl-end --
 
+-- object: i_param_code | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_param_code CASCADE;
+CREATE UNIQUE INDEX i_param_code ON public.tbl_parameters
+	USING btree
+	(
+	  code
+	);
+-- ddl-end --
+
+-- object: i_param_cat | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_param_cat CASCADE;
+CREATE UNIQUE INDEX i_param_cat ON public.tbl_cat_params
+	USING btree
+	(
+	  id_category,
+	  id_parameter
+	);
+-- ddl-end --
+
+-- object: i_p_category | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_p_category CASCADE;
+CREATE INDEX i_p_category ON public.tbl_cat_params
+	USING btree
+	(
+	  id_category
+	);
+-- ddl-end --
+
+-- object: "I_cat_code" | type: INDEX --
+-- DROP INDEX IF EXISTS public."I_cat_code" CASCADE;
+CREATE UNIQUE INDEX "I_cat_code" ON public.tbl_communication_categories
+	USING btree
+	(
+	  code
+	);
+-- ddl-end --
+
+-- object: i_io_table | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_io_table CASCADE;
+CREATE UNIQUE INDEX i_io_table ON public.tbl_io_communication_objects_references
+	USING btree
+	(
+	  table_name
+	);
+-- ddl-end --
+
+-- object: i_author | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_author CASCADE;
+CREATE INDEX i_author ON public.tbl_io_communication_objects_references
+	USING btree
+	(
+	  id_author
+	);
+-- ddl-end --
+
+-- object: i_category | type: INDEX --
+-- DROP INDEX IF EXISTS public.i_category CASCADE;
+CREATE INDEX i_category ON public.tbl_io_communication_objects_references
+	USING btree
+	(
+	  id_category
+	);
+-- ddl-end --
+
 -- object: fk_category_ref | type: CONSTRAINT --
 -- ALTER TABLE public.tbl_communication_categories DROP CONSTRAINT IF EXISTS fk_category_ref CASCADE;
 ALTER TABLE public.tbl_communication_categories ADD CONSTRAINT fk_category_ref FOREIGN KEY (id_child)
