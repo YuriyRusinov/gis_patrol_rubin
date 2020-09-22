4e306d07dbd1da33a5470143358184fbdebee4a3
Yury Rusinov <yrusinov@gmail.com>
2020-09-22 23:15:42 +0300

    Database init for postgresql was corrected.
    Signed-off-by: Yury Rusinov <yrusinov@gmail.com>

--- gis_patrol_server/internals/functions/init_data/users.sql
+++ gis_patrol_server/internals/functions/init_data/users.sql
@@ -2,6 +2,6 @@
 Системные пользователи
 */
 select safeCreateUser('yuriy', 'yuriy', true);
-insert into users(id, id_maclabel, firstname, surname, lastname, family_name, information, email, db_user) values(1, 1, 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yuriy');
+insert into users(id, role_name, id_maclabel, firstname, surname, lastname, family_name, information, email, db_user) values(1, 'yuriy', 1, 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yuriy');
 
-select setval('users_id_seq', 1, true);
+select setval('patrol_roles_id_seq', 1, true);
