4b9cbc77367914f314283af805c17a1f8f1dfcd9
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-12-29 10:16:10 +0300

    For postgres server functions were corrected.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- gis_patrol_server/internals/functions/init_data/maclabels.sql
+++ gis_patrol_server/internals/functions/init_data/maclabels.sql
@@ -1,14 +1,13 @@
 --for linter
-
+/*
 insert into maclabels (id, name, mac_value) values (1, 'Несекретно', '{0,0}'::maclabel);
 insert into maclabels (id, name, mac_value) values (2, 'Cекретно', '{1,0}'::maclabel);
 insert into maclabels (id, name, mac_value) values (3, 'Сов. секретно', '{2,0}'::maclabel);
+*/
 
 
-/*
 --for postgres
 insert into maclabels (id, name, mac_value) values (1, 'Несекретно', (0,0));
 insert into maclabels (id, name, mac_value) values (2, 'Cекретно', (1,0));
 insert into maclabels (id, name, mac_value) values (3, 'Сов. секретно', (2,0));
-*/
 
--- gis_patrol_server/internals/functions/init_data/users.sql
+++ gis_patrol_server/internals/functions/init_data/users.sql
@@ -1,7 +1,7 @@
 /*
 Системные пользователи
 */
-select safeCreateUser('yrusinov', 'yrusinov', true);
-insert into users(id, id_maclabel, role_name, firstname, surname, lastname, family_name, information, email, db_user) values( 1, 1, 'yrusinov', 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yrusinov');
+select safeCreateUser('yuriy', 'yuriy', true);
+insert into users(id, role_name, id_maclabel, firstname, surname, lastname, family_name, information, email, db_user) values(1, 'yuriy', 1, 'Yury', 'Leonovich', 'Rusinov', 'Rusinov Yu.L.', 'Администратор системы', 'Y.L.Rusinov@rubin-spb.ru', 'yuriy');
 
 select setval('patrol_roles_id_seq', 1, true);
